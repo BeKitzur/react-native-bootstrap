f8e6a326030f85ac51f0723ca82205f0ae57e43e
Alexandr Solovyov <alex.solovyov.work@gmail.com>
2017-06-23 12:49:28 +0300

    Configure firebase

---
+++ app/js/firebase.js
@@ -0,0 +1,8 @@
+import * as firebase from "firebase";
+
+let appFirebase = firebase.initializeApp({
+    apiKey: 'AIzaSyAEG1Q7QLhGsy58NNTnjCMqt9tS1MdWoro',
+    // authDomain: 'reactnativebootstrap-5c9ed.firebase.com'
+});
+
+export default appFirebase;
